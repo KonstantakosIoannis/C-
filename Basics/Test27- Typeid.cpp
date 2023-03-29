b7c793756a1be4ac3d7db416df1936dcd16fb6dd
KonstantakosIoannis <128243262+KonstantakosIoannis@users.noreply.github.com>
2023-03-29 10:12:26 +0300

    Update Test27- Typeid.cpp

--- Basics/Test27- Typeid.cpp
+++ Basics/Test27- Typeid.cpp
@@ -15,15 +15,15 @@ class dog :public animal
 
 int main()
 {
-	// MONO me visual studio (protinetai)
+	
 
 	animal* aPTR = new dog;
 
 	cout << typeid(aPTR).name() << endl;
-	cout << typeid(*aPTR).name() << endl;  // kane tsek me kai xoris virtual
+	cout << typeid(*aPTR).name() << endl;  //check with and without virtual 
 
 	cout << (typeid(animal) == typeid(*aPTR)) << endl;
-	// edw tsakarei me ena true-false an to *aPTR einai tipou animal
+
 
 
 
